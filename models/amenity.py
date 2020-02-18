1b739d845344d4e6eed862ec9117b55d36bd996e
Felipe <felipe_londono1@hotmail.com>
2020-02-18 21:38:39 +0000

    [Fix]: Adding modules comment

--- models/amenity.py
+++ models/amenity.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+''' Amenity Modules '''
 from models.base_model import BaseModel
 
 
--- models/state.py
+++ models/state.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+''' State Modules'''
 from models.base_model import BaseModel
 
 
