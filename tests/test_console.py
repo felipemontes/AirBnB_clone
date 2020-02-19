6fd3d1d8350484438410bf5119b3ceba2d1e6062
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-02-19 21:12:44 +0000

    [Update]: Adding some tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
-""" Unittest for console """
+'''Unittest Console Modules'''
+
+import unittest
+import pep8
+
+
+def TestConsole(unittest.TestCase):
+    '''Unittest for console'''
+
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['console.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+if __name__ == '__main__':
+    unittest.main()
