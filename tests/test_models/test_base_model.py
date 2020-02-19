832120e3bed53d82b1cb06128d6a1864cf9a6ca1
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-02-19 17:33:52 +0000

    [Update]: Adding some tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,2 +1,33 @@
 #!/usr/bin/python3
-""" Unittest for BaseModel """
+'''Unittest BaseModel Modules'''
+
+import unittest
+import os
+from models.base_model import BaseModel
+from datetime import datetime
+
+class TestBaseModel(unittest.TestCase):
+    '''Unittest for BaseModel'''
+
+    my_model = BaseModel()
+
+    def testPass(self):
+        '''Tests for attributes'''
+        self.my_model.name = 'Holberton'
+        self.my_model.my_number = 89
+        self.my_model.save()
+
+    def testValues(self):
+        '''Tests for values in json'''
+        my_model_json = self.my_model.to_dict()
+        self.assertTrue(isinstance(self.my_model, BaseModel))
+        self.assertEqual('BaseModel', my_model_json['__class__'])
+        self.assertEqual(self.my_model.id, my_model_json['id'])
+        self.assertEqual(self.my_model.name, my_model_json['name'])
+        self.assertEqual(self.my_model.my_number, my_model_json['my_number'])
+
+    def testInstances(self):
+        '''comment'''
+        self.assertIsInstance(self.my_model.id, str)
+        self.assertIsInstance(self.my_model.created_at, datetime)
+        self.assertIsInstance(self.my_model.updated_at, datetime)
